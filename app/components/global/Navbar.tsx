103c8b863571fd22dd74d903f2384f19b66e0dc8
Marco De Lucca <madeluccar@hotmail.com>
2024-05-17 17:52:10 -0400

    Your commit message

--- app/components/global/Navbar.tsx
+++ app/components/global/Navbar.tsx
@@ -7,14 +7,14 @@ export default function Navbar() {
     <header className="py-6 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Link href="/">
-          <Image src={Logo} width={25} height={25} alt="logo" />
+          <Image src={Logo} width={155} height={155} alt="logo" />
         </Link>
         <nav>
           <ul className="flex items-center gap-x-8">
             <li>
               <Link
                 href="/about"
-                className="hover:text-purple-400 duration-300"
+                className="hover:text-blue-500 duration-300"
               >
                 About
               </Link>
@@ -22,7 +22,7 @@ export default function Navbar() {
             <li>
               <Link
                 href="/projects"
-                className="hover:text-purple-400 duration-300"
+                className="hover:text-blue-500 duration-300"
               >
                 Projects
               </Link>
