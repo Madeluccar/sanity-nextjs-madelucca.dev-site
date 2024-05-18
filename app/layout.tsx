62ed1b4c7ac4dddfe0e97354234747329ae8e575
Marco De Lucca <madeluccar@hotmail.com>
2024-05-17 21:56:02 -0400

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Footer from "./components/global/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sanity Next.js Portfolio Site",
+  title: " Madelucca Dev Sanity Next.js Portfolio Site",
   description: "A personal portfolio site built with Sanity and Next.js",
   openGraph: {
     images: "add-your-open-graph-image-url-here",
