da7541b94973961bf7d02018b7f08d72f69a0e2f
Marco De Lucca <madeluccar@hotmail.com>
2026-09-14 21:43:56 -0400

    Move the homepage artwork a little lower on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -63,9 +63,9 @@ export default async function Home() {
                 </div>
               </div>
             ))}
-          {/* Reserve 80% of the SVG height to account for its 20% upward shift. */}
-          <div aria-hidden="true" className="pointer-events-none relative -z-10 aspect-[1273/724.8]">
-            <div className="absolute inset-x-0 top-0 -translate-y-[20%]">
+          {/* Reserve the visible height after shifting up 10% on mobile and 20% on desktop. */}
+          <div aria-hidden="true" className="pointer-events-none relative -z-10 aspect-[1273/815.4] lg:aspect-[1273/724.8]">
+            <div className="absolute inset-x-0 top-0 -translate-y-[10%] lg:-translate-y-[20%]">
               <HeroSvg />
             </div>
           </div>
